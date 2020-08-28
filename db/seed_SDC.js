5b9b5cd3f1dba75471dc4f3ac8813f6a36e37ae1
andyoe <oe.andy0102@gmail.com>
2020-08-27 21:45:21 -0500

    add generation script for joinTable

--- db/seed_SDC.js
+++ db/seed_SDC.js
@@ -4,13 +4,17 @@ const fs = require('fs');
 
 
 const writeDescriptions = fs.createWriteStream('descriptions.csv');
+const writeJoinTable = fs.createWriteStream('JoinTable.csv');
 
 writeDescriptions.write('id,descriptions,release_date\n', 'utf8');
+writeJoinTable.write('id,id_game,id_developer,id_publisher,id_platform\n','utf8');
+
 
 
 function writeTenMillionDescriptions(writer, encoding, callback) {
-  let i = 10000000;
-  let id = 1;
+  // let i = 10000000;
+  let i = 1000
+  let id = 0;
   let msIn10Years = 1000 * 60 * 60 * 24 * 365 * 10;
 
 
@@ -35,10 +39,55 @@ function writeTenMillionDescriptions(writer, encoding, callback) {
   write();
 }
 
+
+function writeTenMillionJoinTables(writer, encoding, callback) {
+  // let i = 10000000;
+  let i = 1000;
+  let id = 0;
+  let primaryKey = 0;
+  let platformIds = [1, 2, 3];
+
+
+
+  function write() {
+    let ok = true;
+    do {
+      i -= 1;
+      id += 1;
+
+
+      let seedArray = new Array(Math.ceil(Math.random() * 3)).fill(id);
+      for(let j = 0; j<seedArray.length ; j++){
+        primaryKey++
+        let developer = Math.ceil(Math.random() * 50);
+        let publisher =  Math.ceil(Math.random() * 50);
+        let randIdx = Math.floor(Math.random() * platformIds.length);
+
+        let data = `${primaryKey},${id},${developer},${publisher},${randIdx}\n`
+
+        if (i === 0) {
+          writer.write(data, encoding, callback);
+        } else {
+          ok = writer.write(data, encoding);
+        }
+      }
+
+      
+    } while (i > 0 && ok);
+    if (i > 0) {
+      writer.once('drain', write);
+    }
+  }
+  write();
+}
+
+
+
 writeTenMillionDescriptions(writeDescriptions,'utf-8',() =>{
     writeDescriptions.end();
-  
 });
 
-
+writeTenMillionJoinTables(writeJoinTable,'utf-8',() =>{
+  writeJoinTable.end();
+})
 
